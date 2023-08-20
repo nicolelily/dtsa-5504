dc16d7b172ac3329749776b71d553521348c38c5
Nicole Mark <nicolemark@gmail.com>
2023-08-20 15:34:37 -0400

    Update summary_stats.py
    Updated docstring

--- summary_stats.py
+++ summary_stats.py
@@ -3,12 +3,8 @@ def calculate(dataFile, col_num):
     Input Parameters:
         dataFile: The dataset file.
         ithAttre: The ith attribute for which the various properties must be calculated.
-
-    Default value of 0,infinity,-infinity are assigned to all the variables as required. 
-    """
-    numObj, minValue, maxValue, mean, stdev, Q1, median, Q3, IQR = [0,"inf","-inf",0,0,0,0,0,0]
-    #YOUR TASK: Write code to assign the values to the respective variables.
-    dataFile = pd.read_csv('data/dataset.csv')
+   """
+    dataFile = pd.read_csv(dataFile)
     ithAttre = dataFile.iloc[:, col_num]
     
     for col_num in dataFile:
